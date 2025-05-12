7e255164cd873c3b38799466b9d72c5601a15587
enriquegarciaelizaga <quiquegarcia97@gmail.com>
2025-05-12 14:55:47 +0100

    cuarto commit-hasta sección login

--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -9,5 +9,12 @@ export function HomeTemplate() {
 }
 
 const Container = styled.div`
- 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+  background-color: ${({theme})=>theme.bgtotal};
+  color: ${({theme})=>theme.text};
+  width: 100%;
 `
